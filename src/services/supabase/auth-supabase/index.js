b04c2534a3c526811606e14185ae991121ae0a5f
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-12-08 18:30:05 +0100

    page(auth): Check the email entered.

--- src/services/supabase/auth-supabase/index.js
+++ src/services/supabase/auth-supabase/index.js
@@ -3,7 +3,9 @@ import { supabase } from "../";
 const checkEmailExists = async (email) => {
 	let response;
 
-	response = await supabase.functions.invoke('check-email-exists');
+	response = await supabase.functions.invoke('check-email-exists', {
+		body: { email: "admin@gmail.com" }
+	});
 	return (response);
 }
 
