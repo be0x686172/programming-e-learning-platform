b67751e5863a25f73eed6a7c9cfa55d1d8ce00e1
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-12-08 10:46:45 +0100

    page(auth): Check the email entered.

--- src/pages/auth-page/index.jsx
+++ src/pages/auth-page/index.jsx
@@ -1,13 +1,9 @@
-import { useState } from "react";
-import { Outlet } from "react-router";
 import TextInputUI from "../../components/ui/inputs-ui/text-input";
 import SubmitButtonUI from "../../components/ui/buttons-ui/submit-button";
 import './style.scss';
+import { NavLink } from "react-router";
 
 const AuthPage = () => {
-
-	const [outletActive, setOutletActive] = useState(false);
-
 	return (
 		<div className="auth-page">
 			<h2>Welcome to <br /> Programming E-Learning Platform.</h2>
@@ -15,7 +11,8 @@ const AuthPage = () => {
 				<TextInputUI text={"Enter your e-mail address"} type={"email"} />
 				<SubmitButtonUI text="Continue" />
 			</form>
-			{outletActive ? <Outlet /> : ''}
+			<hr />
+			<p className="terms-use-and-private-policy-message">By registaring, you consent to our <span><NavLink>Terms <br /> of Use</NavLink></span> and <span><NavLink>Privacy Policy</NavLink></span>.</p>
 		</div>
 	);
 };
