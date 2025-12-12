37526530f453d7da8dd967e6b4988bf8f53b75de
be0x686172 <abderahmane.beharrahala@gmail.com>
2025-12-12 18:52:15 +0100

    page(auth): Check the validity of inputs entered

--- src/components/features/auth-page/password-validation-feature/index.jsx
+++ src/components/features/auth-page/password-validation-feature/index.jsx
@@ -4,12 +4,11 @@ import { Check } from 'lucide-react';
 const PasswordValidationFeature = () => {
 	return (
 		<div className="password-validation-feature">
-			<div className='left'>
-				<div><Check size={16} /><p></p></div>
-			</div>
-			<div className='right'>
-
-			</div>
+			<div className='not-valid'><Check size={16} /><p>A lowercase letter</p></div>
+			<div><Check size={16} /><p>A capital (uppercase) letter</p></div>
+			<div><Check size={16} /><p>A number</p></div>
+			<div><Check size={16} /><p>A special character ()</p></div>
+			<div><Check size={16} /><p>Minimum 8 characters</p></div>
 		</div>
 	);
 };
